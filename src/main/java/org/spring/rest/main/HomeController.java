18236473dce5714b6a5feae5e9c17f354cfaaa84
unknown <roma.khimani88@yahoo.co.in>
2019-03-02 23:03:28 +0530

    Add Rest API

--- src/main/java/org/spring/rest/main/HomeController.java
+++ src/main/java/org/spring/rest/main/HomeController.java
@@ -1,14 +1,58 @@
 package org.spring.rest.main;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
 
 @RestController
 public class HomeController {
+
+    BlogMockedData blogMockedData = BlogMockedData.getInstance();
 
     @RequestMapping("/home")
     public String controller() {
         return "Welcome to Controller";
     }
+
+    @GetMapping("/blog")
+    public List<Blog> index(){
+        return blogMockedData.fetchBlog();
+    }
+
+    @GetMapping("/blog/{id}")
+    public Blog show(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        return blogMockedData.getBlogById(blogId);
+    }
+
+    @PostMapping("/blog/search")
+    public List<Blog> search(@RequestBody Map<String, String> body){
+        String searchTerm = body.get("text");
+        return blogMockedData.searchBlogs(searchTerm);
+    }
+
+    @PostMapping("/blog")
+    public Blog create(@RequestBody Map<String, String> body){
+        int id = Integer.parseInt(body.get("id"));
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogMockedData.createBlog(id, title, content);
+    }
+
+    @PutMapping("/blog/{id}")
+    public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
+        int blogId = Integer.parseInt(id);
+        String title = body.get("title");
+        String content = body.get("content");
+        return blogMockedData.updateBlog(blogId, title, content);
+    }
+
+    @DeleteMapping("blog/{id}")
+    public boolean delete(@PathVariable String id){
+        int blogId = Integer.parseInt(id);
+        return blogMockedData.delete(blogId);
+    }
+
 
 }
